671f6bd26bde22ec0a3a74d60fcd26c0970aeec6
suleimana <suleiman.alrosan@gmail.com>
2017-01-29 09:49:52 -0500

    APM-start imp

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-
 @Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+    selector : 'pm-app',
+    template : `
+    <div>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <a class="navbar-brand">{{pageTitle}}</a>
+                <ul class="nav navbar-nav">
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Product List</a></li>                    
+                </ul>
+            </div>
+        </nav>
+    </div>    
+    <div class="container">
+        <router-outlet></router-outlet>
+    </div>    
+    `    
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTitle: string = 'Acme Product Mangement'
+}
--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+
+
+import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+
+import { ProductFilterPipe } from './products/product.filter.pipe';
+import { StarComponent } from './shared/star.component';
+
+import { HttpModule } from '@angular/http';
+
+import {ProductModule } from './products/product.module';
+
+
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [    
+    BrowserModule,    
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
+  ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent           
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
